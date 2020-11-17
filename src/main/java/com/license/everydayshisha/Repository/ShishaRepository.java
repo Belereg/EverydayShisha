4df7822689121e8d6e17d4341d0ddf6ef158f72c
Alexandru Nastase <nalexandru27@yahoo.com>
2020-11-18 01:30:48 +0200

    Commmit test 1

--- src/main/java/com/license/everydayshisha/Repository/ShishaRepository.java
+++ src/main/java/com/license/everydayshisha/Repository/ShishaRepository.java
@@ -1,4 +1,9 @@
 package com.license.everydayshisha.Repository;
 
-public interface ShishaRepository {
+import com.license.everydayshisha.Model.DAO.Shisha;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShishaRepository extends JpaRepository<Shisha,Integer> {
 }
