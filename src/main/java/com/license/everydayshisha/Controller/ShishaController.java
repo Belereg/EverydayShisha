340eece8b6a83e2991572ed08bfe9fe7214db637
Alexandru Nastase <nalexandru27@yahoo.com>
2020-11-18 01:35:24 +0200

    Commit test 3

--- src/main/java/com/license/everydayshisha/Controller/ShishaController.java
+++ src/main/java/com/license/everydayshisha/Controller/ShishaController.java
@@ -1,7 +1,9 @@
 package com.license.everydayshisha.Controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/shishas")
 public class ShishaController {
 }
--- src/main/java/com/license/everydayshisha/Service/ShishaService.java
+++ src/main/java/com/license/everydayshisha/Service/ShishaService.java
@@ -1,4 +1,12 @@
 package com.license.everydayshisha.Service;
 
+import com.license.everydayshisha.Repository.ShishaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ShishaService {
+
+    @Autowired
+    private ShishaRepository shishaRepository;
 }
