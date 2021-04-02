4e42ee0ed77c39345ec052aa882ec2ce9e91fb58
Alexandru Nastase <nalexandru27@yahoo.com>
2021-04-02 23:19:05 +0300

    Finished landing page. Made it responsive for all kind of device resolutions.

---
+++ src/main/resources/static/js/hamburgerToggle.js
@@ -0,0 +1,44 @@
+var open = document.getElementById('hamburger');
+var changeIcon = true;
+var cacat = document.getElementById("cacat")
+var hide_instagram = document.getElementById("hide_instagram")
+var hide_facebook = document.getElementById("hide_facebook")
+var hide_snapchat = document.getElementById("hide_snapchat")
+var hide_phone = document.getElementById("hide_phone")
+var hide_whatsapp = document.getElementById("hide_whatsapp")
+
+open.addEventListener("click", function(){
+
+    var overlay = document.querySelector('.overlay');
+    var nav = document.querySelector('nav');
+    var icon = document.querySelector('.menu-toggle i');
+
+    overlay.classList.toggle("menu-open");
+    nav.classList.toggle("menu-open");
+
+
+
+
+    if (changeIcon) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+        cacat.style.display = "none";
+        hide_instagram.style.display = "none";
+        hide_facebook.style.display = "none";
+        hide_snapchat.style.display = "none";
+        hide_phone.style.display = "none";
+        hide_whatsapp.style.display = "none";
+        changeIcon = false;
+    }
+    else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+        cacat.style.display = "flex";
+        hide_instagram.style.display = "inline-block";
+        hide_facebook.style.display = "inline-block";
+        hide_snapchat.style.display = "inline-block";
+        hide_phone.style.display = "inline-block";
+        hide_whatsapp.style.display = "inline-block";
+        changeIcon = true;
+    }
+});
